16766eafee50e0836e98ea2803f8cec0099d79f8
Patrick <ruomungai@gmail.com>
2022-06-17 15:12:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // iterate over all elements in array 
+  for (let i = 0; i < array.length; i++){
+    // for every array[i], iterate over the rest of element (array[j])
+    for (let j = i + 1; j < array.length; j++){
+      // test if array[i] + array [j] == target
+      if(array[i] + array[j] == target){
+        return true;
+      }
+    }
+  }
+  // if addition of any of the two elements in array is not == target, return false
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  0(n*2)
 */
 
 /* 
   Add your pseudocode here
+  iterate over the array
+  add idividual items
+  return true is the addition is equal to target
 */
 
 /*
